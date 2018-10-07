42696b309932bb9deeac65ab703a07a31421473f
tsotumu <tsotumu@qq.com>
2018-10-07 19:29:39 +0800

    修改启动页。

--- app/src/main/java/com/macojia/leanproduct/ui/SplashActivity.java
+++ app/src/main/java/com/macojia/leanproduct/ui/SplashActivity.java
@@ -21,10 +21,6 @@ import butterknife.Bind;
  * on 2016.09.15:16
  */
 public class SplashActivity extends BaseActivity {
-    @Bind(R.id.iv_logo)
-    ImageView ivLogo;
-    @Bind(R.id.tv_name)
-    TextView tvName;
 
     @Override
     public int getLayoutId() {
@@ -39,39 +35,14 @@ public class SplashActivity extends BaseActivity {
     @Override
     public void initView() {
         SetTranslanteBar();
-        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0.3f, 1f);
-        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.3f, 1f);
-        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.3f, 1f);
-        ObjectAnimator objectAnimator1 = ObjectAnimator.ofPropertyValuesHolder(tvName, alpha, scaleX, scaleY);
-        ObjectAnimator objectAnimator2 = ObjectAnimator.ofPropertyValuesHolder(ivLogo, alpha, scaleX, scaleY);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(objectAnimator1, objectAnimator2);
-        animatorSet.setInterpolator(new AccelerateInterpolator());
-        animatorSet.setDuration(2000);
-        animatorSet.addListener(new Animator.AnimatorListener() {
+        getWindow().getDecorView().postDelayed(new Runnable() {
             @Override
-            public void onAnimationStart(Animator animator) {
+            public void run() {
 
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
                 ActivityUtil.startActivity(SplashActivity.this, LoginActivity.class);
                 finish();
             }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        animatorSet.start();
+        }, 3000);
     }
 
 }
